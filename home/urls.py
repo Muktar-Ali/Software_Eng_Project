52582fc8105f4ce7afd96cdd369f23d4fe29acfa
Tijuan03 <tijuanjohnson03@gmail.com>
2025-02-20 21:49:55 -0500

    Comments
    Made comments to home/views and home/urls

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,11 @@
+#importing path
 from django.urls import path
+#importing views from current app
 from . import views 
 
 urlpatterns = [
+    #This maps the root URL to the HomeView
+    #The .as_view() method is needed to convert the view into a usable function
+    #reverse URL lookups in templates and views are allowed because of name='home'
     path('', views.HomeView.as_view(), name='home'),
 ]
