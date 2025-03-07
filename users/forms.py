71a662d5d4720a659462c98ad58506aa3b88b9a3
Tijuan03 <tijuanjohnson03@gmail.com>
2025-03-06 20:59:50 -0500

    Comments
    Made comments to a few html files as well as some files in "Users".

--- users/forms.py
+++ users/forms.py
@@ -6,19 +6,24 @@ from django.contrib.auth.hashers import make_password
 class SignupForm(forms.ModelForm):
     class Meta:
         model = CustomUser
+        #define fields to be included in form
         fields = ['username', 'first_name', 'last_name', 'gender', 'age', 'email', 'password', 'height', 'weight', 'activity_level']
+        #specify custom input types for specific fields
         widgets = {
             'password': forms.PasswordInput(),
         }
+        #custom labels for the form fields
         labels = {
             'weight': 'Weight (pounds):',
             'height': 'Height (inches):',
             'age': 'Age (years):',
         }
         def save(self, commit=True):
+            #create a user object from the form data without saving
             user = super().save(commit=False)
             # Hash the password before saving
             user.password = make_password(user.password)
             if commit:
+                #save the user object to the database
                 user.save()
             return user
