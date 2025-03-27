64057958ba4aed42efa4e119c8ac9ee7a10e8cf2
Shadae Page <shadaeapage23@gmail.com>
2025-03-27 15:51:30 -0400

    New Test Cases
    I added 3 test cases. One for invalid login credentials, one for duplicate username and another for profile update.

--- home/tests.py
+++ home/tests.py
@@ -53,4 +53,63 @@ class UserTestCase(TestCase):
         form.save()
         self.client.login(username="ThirdTestUser", password="ThreeTestUser")
         self.client.logout()
+    def test_DuplicateUsernameTest(self):
+    # Create a user first
+        dict = {
+        'username': "duplicateUser",
+        'first_name': "Duplicate",
+        'last_name': "User",
+        'email': "duplicate@gmail.com",
+        'password': "DuplicateUser123",
+        'gender': 'M',
+        'age': 30,
+        'height': 68,
+        'weight': 160,
+        'activity_level': "Heavy",
+         }
+        form = SignupForm(dict)
+        form.save()  # First save should work
+        # Try creating another user with the same username
+        duplicate_data = dict.copy()
+        duplicate_data['username'] = "duplicateUser"  # Change email to avoid duplicate email error
+        duplicate_form = SignupForm(duplicate_data)
+        self.assertFalse(duplicate_form.is_valid())  # Should fail due to duplicate username
+    def test_UserProfileUpdateTest(self):
+        user_data = {
+            'username': "updateUser",
+            'first_name': "Update",
+            'last_name': "User",
+            'email': "updateuser@gmail.com",
+            'password': "UpdateUser123",
+            'gender': 'M',
+            'age': 35,
+            'height': 70,
+            'weight': 180,
+            'activity_level': "Heavy",
+        }
+        form = SignupForm(user_data)
+        form.save()
+        # Login and update profile
+        self.client.login(username="updateUser", password="UpdateUser123")
+        updated_data = {'first_name': "Updated", 'age': 36}
+        response = self.client.post('/profile/update/', updated_data)  # Adjust URL as needed
+        user = CustomUser.objects.get(username="updateUser")
+        self.assertEqual(user.first_name, "Update")  # Check if update was successful
+    def test_InvalidLoginTest(self):
+        user_data = {
+            'username': "validUser",
+            'first_name': "Valid",
+            'last_name': "User",
+            'email': "validuser@gmail.com",
+            'password': "ValidPassword123",
+            'gender': 'F',
+            'age': 28,
+            'height': 64,
+            'weight': 130,
+            'activity_level': "Light",
+        }
+        form = SignupForm(user_data)
+        form.save()
+        # Try logging in with wrong password
+        self.assertFalse(self.client.login(username="validUser", password="WrongPassword"))
     
